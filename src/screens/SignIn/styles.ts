49a5283dde48d8725eda6c2e82af67db434df9fa
Helder Correia <helder.patrick25@gmail.com>
2021-12-12 19:56:20 +0000

    add Inputs components

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,42 +1,34 @@
+import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
-import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
-  ${({ theme }) => css`
-    flex: 1;
-    padding: 0 24px;
-    background-color: ${theme.colors.background_primary};
-  `}
+  padding: 0 24px;
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
 export const Header = styled.View`
-  ${({ theme }) => css`
-    width: 100%;
-    margin-top: ${getStatusBarHeight() + 116}px;
-  `}
+  width: 100%;
+  margin-top: ${getStatusBarHeight() + 115}px;
 `;
 
 export const Title = styled.Text`
-  ${({ theme }) => css`
-    font-size: ${RFValue(40)}px;
-    font-family: ${theme.fonts.secondary_600};
-    color: ${theme.colors.title};
-  `}
+  font-size: ${RFValue(40)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.title}; 
 `;
 
 export const SubTitle = styled.Text`
-  ${({ theme }) => css`
-    margin-top: 16px;
-    line-height: ${RFValue(25)}px;
-    font-size: ${RFValue(15)}px;
-    font-family: ${theme.fonts.primary_400};
-    color: ${theme.colors.text};
-  `}
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text}; 
+  line-height: ${RFValue(25)}px;
+  margin-top: 16px;
 `;
 
-export const Footer = styled.View`
-  ${({ theme }) => css`
-    width: 100%;
-  `}
+export const Form = styled.View`
+  width: 100%;
+  margin: 64px 0;
 `;
+
+export const Footer = styled.View``;
